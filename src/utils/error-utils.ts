52b8a4c8924e73a3f31af7838b99b1d84fb205ad
chepeLove <ilya.chepelov@gmail.com>
2023-11-27 12:44:11 +0300

    fix: refactoring error handling functions

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -2,6 +2,12 @@ import {setAppErrorAC, setAppStatusAC} from "../reducers/app-reducer";
 import {ResponseType} from "../api/api";
 import {Dispatch} from "redux";
 
+export type ErrorType = {
+    "statusCode": number,
+    "messages": string[],
+    "error": string
+}
+
 export const handleServerAppError = <D>(data:ResponseType<D>,dispatch:Dispatch)=>{
     if(data.messages.length){
         dispatch(setAppErrorAC(data.messages[0]))
@@ -11,7 +17,7 @@ export const handleServerAppError = <D>(data:ResponseType<D>,dispatch:Dispatch)=
     dispatch(setAppStatusAC('failed'))
 }
 
-export const handleServerNetworkError = (error:any,dispatch:Dispatch) =>{
-    dispatch(setAppErrorAC(error.message ? error.message : 'Some error occured'))
+export const handleServerNetworkError = (dispatch:Dispatch,error:string) =>{
+    dispatch(setAppErrorAC(error ? error : 'Some error occurred'))
     dispatch(setAppStatusAC('failed'))
 }
