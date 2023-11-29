94a59c44bc0319953377e28961ce73b11fd30c1b
chepeLove <ilya.chepelov@gmail.com>
2023-11-29 18:02:05 +0300

    fix: write new styles

--- src/components/linearProgress/LinearProgress.tsx
+++ src/components/linearProgress/LinearProgress.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import s from './LinearProgress.module.css'
 export const LinearProgress = () => {
-    return (
-        <div className={s.container}>
-            <div className={s.loader}></div>
-        </div>
-    );
+    return <div className={s.linearLoader}></div>
 };
