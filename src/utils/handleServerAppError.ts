45a6faef95df3b4524239ea610cbbded63a17c53
chepeLove <ilya.chepelov@gmail.com>
2023-12-14 15:19:10 +0300

    refactor: change Dispatch to AppDispatch

--- src/utils/handleServerAppError.ts
+++ src/utils/handleServerAppError.ts
@@ -1,6 +1,6 @@
 import { ResponseType } from "api/api";
-import { Dispatch } from "redux";
 import { appActions } from "reducers/app-reducer";
+import { AppDispatch } from "store/store";
 
 export type ErrorType = {
   statusCode: number;
@@ -8,16 +8,11 @@ export type ErrorType = {
   error: string;
 };
 
-export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch) => {
+export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: AppDispatch) => {
   if (data.messages.length) {
     dispatch(appActions.setAppError({ error: data.messages[0] }));
   } else {
     dispatch(appActions.setAppError({ error: "Some error occurred" }));
   }
-  dispatch(appActions.setAppStatus({ status: "failed" }));
-};
-
-export const handleServerNetworkError = (dispatch: Dispatch, error: string) => {
-  dispatch(appActions.setAppError({ error: error ? error : "Some error occurred" }));
   dispatch(appActions.setAppStatus({ status: "failed" }));
 };
